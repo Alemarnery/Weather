3e25c24ae8102e7ece9db80b8c72ff5f315c9668
alemarnery <aleemar.95@gmail.com>
2021-04-20 14:43:16 -0430

    weather

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -28,7 +28,7 @@ class SearchBar extends Component {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <input
-          placeholder="Get a five-day forecast in your favorite city"
+          placeholder="Get a five-day forecast in USA city"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange}
